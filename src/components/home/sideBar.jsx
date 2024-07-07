9345acac4224bd25b29afff285d93ac899fecc6a
Breysisalazar <Breysisalazar@utec.edu.pe>
2024-07-07 17:43:15 -0500

    ...

--- src/components/home/sideBar.jsx
+++ src/components/home/sideBar.jsx
@@ -34,10 +34,7 @@ export default function SideBarHomePage() {
             <Sidebar visible={visible} onHide={() => setVisible(false)}>
                 <h2>Sidebar</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.
+                    
                 </p>
             </Sidebar>
             { }
